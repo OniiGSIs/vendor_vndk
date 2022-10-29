e3bcbd4688377a6f41f9875a2587771ad33e6c1c
ZualoliconVN <suchi12212211@gmail.com>
2022-10-29 12:52:10 +0700

    new apex thing

--- make13.sh
+++ make13.sh
@@ -31,9 +31,6 @@ ln -s  /apex/com.android.vndk.v30/lib64 $systempath/lib64/vndk-sp-30
 ln -s  /apex/com.android.vndk.v31/lib64 $systempath/lib64/vndk-sp-31
 ln -s  /apex/com.android.vndk.v32/lib64 $systempath/lib64/vndk-sp-32
 
-# Extract VNDK apex to system
-7z x -y $thispath/13/com.android.vndk.v32.apex.7z -o$systempath/apex/ 2>/dev/null >> $systempath/zip.log
-7z x -y $thispath/13/com.android.vndk.v31.apex.7z -o$systempath/apex/ 2>/dev/null >> $systempath/zip.log
-7z x -y $thispath/13/com.android.vndk.v29.apex.7z -o$systempath/apex/ 2>/dev/null >> $systempath/zip.log
-7z x -y $thispath/13/com.android.vndk.v30.apex.7z -o$systempath/apex/ 2>/dev/null >> $systempath/zip.log
+# Extract all apex to system
+7z x -y $thispath/13/*.7z -o$systempath/apex/ 2>/dev/null >> $systempath/zip.log
 rm -rf $systempath/zip.log
